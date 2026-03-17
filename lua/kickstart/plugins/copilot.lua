dc9fe38ef48b5dc1a60b16b2e7c8d2a8c368f01f
Scott Cole <scott.cole@rockstarleeds.com>
2026-03-17 11:05:50 +0000

    nvim

---
+++ lua/kickstart/plugins/copilot.lua
@@ -0,0 +1,10 @@
+return {
+  {
+    'zbirenbaum/copilot.lua',
+    event = 'InsertEnter',
+    opts = {
+      suggestion = { enabled = false }, -- disable ghost text
+      panel = { enabled = false },
+    },
+  },
+}
